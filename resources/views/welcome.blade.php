d5b92bcd8e626a2b6815afcd249e985479584dee
Gayan Kavinda <gayan982822513.lk@gmail.com>
2023-12-11 09:25:08 +0530

    Source Code Push - 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ministry of Internal & Home Affairs and Provincial Councils & Local Government</title>
+    <link rel="icon" href="{{ asset('img/gov-logo.png') }}" type="image/x-icon">
+
+    <!-- Box Icons -->
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <!-- <link href="{{ asset('css/unpkg_boxicon.css')}}" rel="stylesheet"> -->
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+
+<body>
+    <!-- header Design -->
+    <header class="header">
+        <a href="{{ asset('img/gov-logo.png') }}" class="logo"></a>
+        <i class='bx bx-menu-alt-left' id="menu-icon"></i>
+
+        <nav class="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#testing1">Testing1</a>
+            <a href="#testing2">Testing2</a>
+            <a href="#testing1">Testing3</a>
+            <a href="#testing2">Testing4</a>        
+        </nav>
+    </header> 
+
+    <!-- Home Section Design -->
+
+</body>
+</html>
